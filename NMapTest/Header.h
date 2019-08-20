6a9fdce778a8ae9f37f5b01c5e0304c1a2ae111c
lsy044 <2sy100@likelion.org>
2019-08-20 18:28:57 +0900

    NmapInit

---
+++ NMapTest/Header.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  NMapTest
+//
+//  Created by JIN on 20/08/2019.
+//  Copyright © 2019 sy. All rights reserved.
+//
+#ifndef your_project_Bridging_Header_h
+#define your_project_Bridging_Header_h
+
+#import <NMapViewerSDK/NMapView.h>
+#import <NMapViewerSDK/NMapLocationManager.h>
+
+#endif /* Header_h */
