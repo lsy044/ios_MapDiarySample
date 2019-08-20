f0b3ae628b0fbd4394c446838c0022d12e39bff3
lsy044 <2sy100@likelion.org>
2019-08-20 18:57:14 +0900

    Cocoapod설치

---
+++ NMapTest/SearchPlaceViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchPlaceViewController.swift
+//  NMapTest
+//
+//  Created by JIN on 20/08/2019.
+//  Copyright © 2019 sy. All rights reserved.
+//
+
+import UIKit
+
+class SearchPlaceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
---
+++ Pods/Target Support Files/Pods-NMapTestTests/Pods-NMapTestTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NMapTestTests : NSObject
+@end
+@implementation PodsDummy_Pods_NMapTestTests
+@end
